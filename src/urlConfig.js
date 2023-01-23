10e09007a58b2dec852e6b63d06140f1fb84ece1
Satvik Raj Patel <gameman777.rp@gmail.com>
2023-01-23 15:47:33 +0530

    second commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,6 @@
-export const api = 'http://localhost:2000/api';
+const baseUrl = "https://ecommerce-gyxw.onrender.com";
+
+export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`;
-}
+    return `${baseUrl}/public/${fileName}`;
+};
