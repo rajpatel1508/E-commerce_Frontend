b5a689939e724bd044e8e301d999f1202b6eeb13
Satvik Raj Patel <gameman777.rp@gmail.com>
2023-01-24 11:56:55 +0530

    menuheader fixed

--- src/containers/search/index.js
+++ src/containers/search/index.js
@@ -26,42 +26,40 @@ export default function Search() {
     }, []);
     return (
         <Layout>
-            <div style={{ display: "flex" ,height: 'auto',overflow: 'hidden',flexWrap: 'wrap'}}>
+            <div style={{ marginLeft: '20px', display: "flex", height: 'auto', overflow: 'hidden', flexWrap: 'wrap',justifyContent: 'flex-start'}}>
                 {data.map((product, key) => {
                     return (
-                        <div>
-                            <Link
-                                to={`/${product.slug}/${product._id}/p`}
-                                style={{
-                                    display: "block",
-                                    textDecoration: "none",
-                                    color: "#000",
-                                    padding: '20px'
-                                }}
-                                className="productContainer"
-                            >
-                                <div className="productImgContainer">
-                                    <img src={generatePublicUrl(product.productPictures[0].img)} alt="" />
+                        <Link
+                            to={`/${product.slug}/${product._id}/p`}
+                            style={{
+                                display: "block",
+                                textDecoration: "none",
+                                color: "#000",
+                                padding: '10px'
+                            }}
+                            className="productContainer"
+                        >
+                            <div className="productImgContainer">
+                                <img src={generatePublicUrl(product.productPictures[0].img)} alt="" />
+                            </div>
+                            <div className="productInfo">
+                                <div style={{ margin: "10px 0" }}>{product.name}</div>
+                                <div>
+                                    <Rating value="4.3" />
+                                    &nbsp;&nbsp;
+                                    <span
+                                        style={{
+                                            color: "#777",
+                                            fontWeight: "500",
+                                            fontSize: "12px",
+                                        }}
+                                    >
+                                        (3353)
+                                    </span>
                                 </div>
-                                <div className="productInfo">
-                                    <div style={{ margin: "10px 0" }}>{product.name}</div>
-                                    <div>
-                                        <Rating value="4.3" />
-                                        &nbsp;&nbsp;
-                                        <span
-                                            style={{
-                                                color: "#777",
-                                                fontWeight: "500",
-                                                fontSize: "12px",
-                                            }}
-                                        >
-                                            (3353)
-                                        </span>
-                                    </div>
-                                    <Price value={product.price} />
-                                </div>
-                            </Link>
-                        </div>
+                                <Price value={product.price} />
+                            </div>
+                        </Link>
                     )
                 })}
             </div>
